ac1867500b7b2a63c324a34b221b26a9e2568a20
Davi Herbes <hmatosdavi@gmail.com>
2024-08-16 01:40:07 -0300

    Adicionando coisa no home

--- src/pages/home/default/index.tsx
+++ src/pages/home/default/index.tsx
@@ -5,14 +5,34 @@ export const DefaultHome = () => {
   return (
     <SDefault className="default-home-page">
       <header className="default-menu">
-        <a href="https://ifrs.edu.br/feliz">
-          <img src={logoIfrs} alt="IFRS" />
-        </a>
+        <div className="default-menu-content">
+          <a className="ifrs-link" href="https://ifrs.edu.br/feliz">
+            <img src={logoIfrs} alt="IFRS" />
+          </a>
+          <nav className="intro-home-nav">
+            <a href="#intro-default-home" className="intro-home-nav-item">
+              Sobre
+            </a>
+            <a href="#user-sign-default-home" className="intro-home-nav-item">
+              Entrar/Cadastrar
+            </a>
+          </nav>
+        </div>
       </header>
       <section id="intro-default-home">
-        <div className="logo-container">
-          <div className="logo">Logo Maneira</div>
-          <span className="creators">Davi Lucas Felipe Lassem e Luft</span>
+        <div className="intro-container">
+          <div className="logo-container">
+            <div className="logo">Logo Maneira</div>
+            <span className="creators">Davi Lucas Felipe Lassem e Luft</span>
+          </div>
+          <div className="about-container">
+            <span className="about">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
+              unde modi quam voluptas quasi id amet ipsam laudantium placeat qui
+              mollitia quia accusantium impedit suscipit maiores aliquid illo,
+              iste nam!
+            </span>
+          </div>
         </div>
       </section>
       <section id="user-sign-default-home"></section>
--- src/pages/home/default/styles.ts
+++ src/pages/home/default/styles.ts
@@ -6,15 +6,43 @@ export const SDefault = styled.div<{ theme: ThemeType }>`
     height: 100vh;
 
     .default-menu {
-      padding-left: 4vw;
-      height: 13vh;
-      width: 100%;
-      align-items: center;
+      z-index: 1;
       position: fixed;
-      img {
-        max-height: 12vh;
-      }
+      width: 100%;
+      background: #fff;
+
+      display: flex;
+      justify-content: center;
       box-shadow: 0 0 5px 5px rgba(0, 0, 0, 0.1);
+
+      .default-menu-content {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 13vh;
+        width: 80%;
+
+        .ifrs-link {
+          display: flex;
+          align-items: center;
+          img {
+            max-height: 12vh;
+          }
+        }
+
+        .intro-home-nav {
+          display: flex;
+          gap: 2vw;
+          .intro-home-nav-item {
+            transition: font-weight 300ms;
+            text-decoration: none;
+            color: #000;
+          }
+          .intro-home-nav-item:hover {
+            font-weight: 330;
+          }
+        }
+      }
     }
 
     section {
@@ -26,23 +54,39 @@ export const SDefault = styled.div<{ theme: ThemeType }>`
       width: 100%;
       padding-top: 13vh;
       align-content: center;
+      padding-bottom: 13vh;
       animation: fadeInAnimation 2s forwards;
 
-      .logo-container {
+      .intro-container {
         margin: 0 auto;
-        height: 30vh;
-        width: 50%;
-        text-align: center;
+        height: 50vh;
+        width: 80%;
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        gap: 3vw;
 
-        .logo {
-          margin-bottom: 2vh;
-          font-family: ${theme.font.family.secondary};
-          font-weight: 900;
-          font-size: ${theme.font.sizes.pageTitle};
+        .logo-container {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          .logo {
+            margin-bottom: 2vh;
+            font-family: ${theme.font.family.secondary};
+            font-weight: 900;
+            font-size: ${theme.font.sizes.pageTitle};
+          }
+          .creators {
+            font-weight: 400;
+            font-size: ${theme.font.sizes.small};
+          }
         }
-        .creators {
-          font-weight: 400;
-          font-size: ${theme.font.sizes.small};
+
+        .about-container {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
         }
       }
     }
